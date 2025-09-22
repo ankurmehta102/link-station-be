08e31bfb1e6fe6bb85331b7a47bb136877dcd002
Ankur Mehta <ankurmehta102@gmail.com>
2025-09-22 06:07:58 +0530

    refactor(link): rename linksentity.ts to link.entity.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { User } from './users/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { LinksModule } from './links/links.module';
-import { Link } from './links/entities/linksentity';
+import { Link } from './links/entities/link.entity';
 
 @Module({
   imports: [
