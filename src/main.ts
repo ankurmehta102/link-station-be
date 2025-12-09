8c23645ae53b8f6bdff417f07f8dada32e9d1dd8
Ankur Mehta <ankurmehta102@gmail.com>
2025-12-09 18:06:08 +0530

    chore(server): enable CORS for development domain

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://link-station.netlify.app'],
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
